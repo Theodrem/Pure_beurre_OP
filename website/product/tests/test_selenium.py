9b441b40e5651e4917423412ae0dbb32aba88f9e
theot <theotim@outlook.fr>
2021-04-06 11:21:12 +0200

    update 3.4 selenium test

--- website/product/tests/test_selenium.py
+++ website/product/tests/test_selenium.py
@@ -1,9 +1,7 @@
 import time
-import pytest
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
+from django.urls import reverse
 from ..models import Product, Category
 from django.contrib.auth.models import User
 
@@ -33,14 +31,46 @@ class MySeleniumTests(StaticLiveServerTestCase):
                                              email="email@outlook.fr",
                                              password="pass_test")
 
-        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.browser = webdriver.Edge(executable_path="C:\exec\msedgedriver.exe")
 
     def tearDown(self):
         self.browser.close()
 
-    def test_no_project_errors(self):
-        a = self.browser.get(self.live_server_url)
-        self.assertEquals(a, "http://127.0.0.1:8000/")
+    def test_research(self):
+        self.client.login(username='username_test', password='pass_test')
+        self.browser.get(self.live_server_url)
+        # find the elements you need to submit form
+        url = self.live_server_url + reverse("results_view") + '?food=fruit'
+        index_form = self.browser.find_element_by_xpath('//*[@id="id_food"]')
+        button_form = self.browser.find_element_by_id('submit')
+        index_form.send_keys("fruit")
+        button_form.click()
+        self.assertEquals(self.browser.current_url, url)
+
+        time.sleep(3)
+
+    def test_login_user(self):
+        self.browser.get(self.live_server_url + "/login/")
+
+        username = self.browser.find_element_by_xpath('//*[@id="id_username"]')
+        passwd = self.browser.find_element_by_xpath('//*[@id="id_password"]')
+        button_form = self.browser.find_element_by_id('submit')
+
+        username.send_keys("username_test")
+        passwd.send_keys("pass_test")
+
+        button_form.click()
+
+        url = self.live_server_url + reverse("dashboard_view", args=["username_test"])
+        self.assertEquals(self.browser.current_url, url)
+
+        time.sleep(5)
+
+
+        
+
+
+
 
 
 
