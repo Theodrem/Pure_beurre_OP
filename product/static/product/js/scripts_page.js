bc154b9347e5976b56945ca4418442402b6e8e77
theot <theotim@outlook.fr>
2021-04-07 11:07:08 +0200

    update settigs

--- product/static/product/js/scripts_page.js
+++
@@ -1,9 +0,0 @@
-var nav = document.querySelector("#mainNav");
-
-window.addEventListener('scroll', () => {
-     if (window.scrollY < 200 ) {
-         nav.classList.add("navbar-scrolled");
-     } else {
-        nav.classList.remove("navbar-scrolled");
-        }
-});
