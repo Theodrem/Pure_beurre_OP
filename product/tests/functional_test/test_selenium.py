ddc7bfd1e8ea83bf3085a37c937cacb2ffdab2eb
theot <theotim@outlook.fr>
2021-04-10 13:30:26 +0200

    update tests

--- product/tests/functional_test/test_selenium.py
+++ product/tests/functional_test/test_selenium.py
@@ -2,7 +2,7 @@ import time
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from django.urls import reverse
-from product.models import Product, Category
+from product.models import Product, Category, Substitute
 from django.contrib.auth.models import User
 
 
@@ -37,33 +37,68 @@ class MySeleniumTests(StaticLiveServerTestCase):
         self.browser.close()
 
     def test_research(self):
-        self.client.login(username='username_test', password='pass_test')
         self.browser.get(self.live_server_url)
         # find the elements you need to submit form
         url = self.live_server_url + reverse("results_view") + '?food=fruit'
-        index_form = self.browser.find_element_by_xpath('//*[@id="id_food"]')
+
+        index_form = self.browser.find_element_by_xpath('/html/body/div/header/div/div/div[2]/form/input')
         button_form = self.browser.find_element_by_id('submit')
+
         index_form.send_keys("fruit")
         button_form.click()
+
+        cat = Category.objects.get(name="fruit")
+        prods = Product.objects.all().filter(category=cat)
+
+        for p in prods:
+            self.assertTrue(p.name in self.browser.page_source)
         self.assertEquals(self.browser.current_url, url)
 
-        time.sleep(3)
+        time.sleep(5)
+
+    def test_save_substitute(self):
+        self.browser.get(self.live_server_url + "/login/")
+
+        username = self.browser.find_element_by_xpath('//*[@id="id_username"]')
+        passwd = self.browser.find_element_by_xpath('//*[@id="id_password"]')
+        button_form = self.browser.find_element_by_xpath('//*[@id="submit"]')
+
+        username.send_keys("username_test")
+        passwd.send_keys("pass_test")
+
+        self.browser.execute_script("arguments[0].click();", button_form)
+
+        form = self.browser.find_element_by_xpath('//*[@id="logo"]')
+        self.browser.execute_script("arguments[0].click();", form)
 
 
-        #add register
 
-        #add save
+        index_form = self.browser.find_element_by_xpath('/html/body/div/header/div/div/div[2]/form/input')
+        button_form_index = self.browser.find_element_by_id('submit')
 
-        #plan de test
+        index_form.send_keys("fruit")
+        button_form_index.click()
 
-        #generer test
+        save = self.browser.find_element_by_xpath('/ html / body / div[2] / div[2] / div[1] / div / div[2] / form / button')
+        self.browser.execute_script("arguments[0].click();", save)
 
-        #nettoyer repo
+        url = self.live_server_url + reverse("my_products")
+        subs = Product.objects.all().filter(substitute__user=self.user).order_by("nutriscore")
+
+        self.assertEquals(self.browser.current_url, url)
+        self.assertEquals(len(subs), 1)
+        for sub in subs:
+            self.assertTrue(sub.name in self.browser.page_source)
+        self.assertEquals(self.browser.current_url, url)
+
+
+        time.sleep(10)
 
 
 
 
+        # plan de test
 
+        # generer test
 
-
-
+        # nettoyer repo
--- user/tests/functional_test/test_selenium.py
+++ user/tests/functional_test/test_selenium.py
@@ -19,14 +19,39 @@ class MySeleniumTests(StaticLiveServerTestCase):
 
         username = self.browser.find_element_by_xpath('//*[@id="id_username"]')
         passwd = self.browser.find_element_by_xpath('//*[@id="id_password"]')
-        button_form = self.browser.find_element_by_id('submit')
+        button_form = self.browser.find_element_by_xpath('//*[@id="submit"]')
 
         username.send_keys("username_test")
         passwd.send_keys("pass_test")
 
-        button_form.click()
+        self.browser.execute_script("arguments[0].click();", button_form)
 
         url = self.live_server_url + reverse("dashboard_view")
         self.assertEquals(self.browser.current_url, url)
 
-        time.sleep(15)
+        time.sleep(5)
+
+    def test_register_user(self):
+        self.browser.get(self.live_server_url + "/register/")
+
+        username = self.browser.find_element_by_xpath('//*[@id="id_username"]')
+        email = self.browser.find_element_by_xpath('//*[@id="id_email"]')
+        passwd = self.browser.find_element_by_xpath('//*[@id="id_password"]')
+        repasswd = self.browser.find_element_by_xpath('//*[@id="id_repassword"]')
+        button_form = self.browser.find_element_by_xpath('//*[@id="submit"]')
+
+        username.send_keys("new_username")
+        email.send_keys("email@hotmail.fr")
+        passwd.send_keys("password")
+        repasswd.send_keys("password")
+        self.browser.execute_script("arguments[0].click();", button_form)
+
+        url = self.live_server_url + reverse("dashboard_view")
+        user = User.objects.get(username="new_username")
+        all_users = User.objects.all()
+
+        self.assertEquals(self.browser.current_url, url)
+        self.assertEquals(user.username, "new_username")
+        self.assertEquals(len(all_users), 2)
+        
+        time.sleep(5)
